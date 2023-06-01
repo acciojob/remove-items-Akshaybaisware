bd51fb48288bb8e84270acee2186e118ac23d6a9
Akshaybaisware <130745994+Akshaybaisware@users.noreply.github.com>
2023-06-01 15:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+window.onload = function()
+	{
+		let colorSelect = document.getElementById("colorSelect");
+		let submit = document.getElementById("submit");
+		submit.addEventListener("click" , function()
+		{
+         let selectedIndex = colorSelect.selectedIndex;
+			if(selectedIndex !== -1)
+			{
+				colorSelect.remove(selectedIndex);
+			}
+		});
+	};
